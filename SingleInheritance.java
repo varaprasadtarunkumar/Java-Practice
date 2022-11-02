c80d90de9a436a1e47de10da5a5d2dcae4eb9d3b
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2022-11-02 22:19:16 +0530

    Add files via upload

---
+++ SingleInheritance.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class Parent{
+    int a=15;
+}
+class Child extends Parent{
+    int b=20;
+public class SingleInheritance{
+    
+}
+public static void main(String args[]){
+ Child ch=new Child();
+ System.out.println("The sum of a(From Parent class) and b(From Child class) is "+(ch.a+ch.b));
+}
+}
+  
