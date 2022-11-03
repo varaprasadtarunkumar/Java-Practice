5f0694a8fae929e0ef7851ea2daef2567c1fd412
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2022-11-03 12:08:35 +0530

    Add files via upload

---
+++ MultipleInheritance.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class Animal{  
+void eat(){
+	System.out.println("eating");
+}  
+}  
+class Dog extends Animal{  
+void bark(){
+	System.out.println("barking");
+}  
+}  
+class BabyDog extends Dog{  
+void weep(){
+	System.out.println("weeping");
+}  
+}  
+class MultipleInheritance{  
+public static void main(String args[]){  
+BabyDog d=new BabyDog();  
+d.weep();  
+d.bark();  
+d.eat();  
+}} 
