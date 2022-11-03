20178ca7fa15183a836158cbc57db0eccd826977
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2022-11-03 09:45:40 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -6,9 +6,8 @@ class Parent{
 }
 class Child extends Parent{
     int b=20;
-public class SingleInheritance{
-    
 }
+public class SingleInheritance{
 public static void main(String args[]){
  Child ch=new Child();
  System.out.println("The sum of a(From Parent class) and b(From Child class) is "+(ch.a+ch.b));
